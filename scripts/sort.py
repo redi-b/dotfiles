f943f6a8b0fade51b23361779f0137f513ec2a59
rediet <rb.rediet@gmail.com>
2021-05-29 17:38:27 +0300

    new script ...

---
+++ scripts/sort.py
@@ -0,0 +1,50 @@
+import os
+
+home_dir = os.path.expanduser("~")
+sort_dir = "wallpapers"   # directory to be sorted
+main_dir = os.path.join(home_dir, sort_dir)
+
+# files should be formatted as
+# prefix + index + extension
+prefix = "wall"
+extension = ".jpg"
+prefix_len = len(prefix)    # this is the where the 'index' begins
+
+# example {"wall4.jpg": "wall3.jpg"}
+# means ""wall4.jpg" should be renamed
+# to "wall3.jpg"
+changes = {}
+
+def check_changes():
+    filenames = os.listdir(main_dir)
+    # First, we split the file extension (jpg).
+    # Then we sort the list by the 'index' which starts
+    # from the 'prefix_len'th index.
+    filenames.sort(key=lambda x: int(x.split(".")[0][prefix_len:]))
+
+    # store the index of each file
+    fileidx = []
+    for i in filenames:
+        fileidx.append(int(i.split(".")[0][prefix_len:]))
+
+    for idx, item in enumerate(fileidx):
+        # making sure the index isn't out of bounds
+        if idx < (len(fileidx) - 1):
+            if (item + 1) == fileidx[idx + 1]:
+                continue
+            # add to changes if the file should be renamed ( {"current_filename": "new_filename"} )
+            # changes[prefix + str(fileidx[idx + 1]) + extension] = prefix = str(item + 1) + extension
+            changes[f"{prefix}{str(fileidx[idx + 1])}{extension}"] = ("wall" + str(item + 1) + ".jpg")
+
+# check for changes and start a loop that
+# applies the first changes, then clears it and
+# checks for changes again. It will continue
+# until no more changes are found
+check_changes()
+while changes:
+
+    for key, item in changes.items():
+        os.system(f"mv -v {main_dir}/{key} {main_dir}/{item}")
+
+    changes.clear() 
+    check_changes()
