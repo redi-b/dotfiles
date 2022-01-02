2a3dc5fc1db2a3ad1cc33c66bbbce094f57e51aa
redi-b <rb.rediet@gmail.com>
2022-01-02 10:07:43 +0300

    .local/src

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -186,6 +186,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_n,      togglealttag,   {0} },
+	{ MODKEY,                       XK_s,      togglesticky,   {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
